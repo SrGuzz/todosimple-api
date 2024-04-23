59bd39a1b3ebf6904bcf033ce2ed45b9ed34ddb8
Albert Luis Pereira de Jesus <albertluis123y88@gmail.com>
2024-04-23 12:16:19 -0300

    Create Tesk model

--- src/main/java/com/example/todosimple/models/User.java
+++ src/main/java/com/example/todosimple/models/User.java
@@ -1,12 +1,13 @@
 package com.example.todosimple.models;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -43,7 +44,8 @@ public class User {
     @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
     private String password;
 
-    //private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
 
     public User() {
@@ -56,6 +58,13 @@ public class User {
         this.password = password;
     }
 
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 
     public Long getId() {
         return this.id;
